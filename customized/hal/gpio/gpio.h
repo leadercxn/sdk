0cf75a20bd8f15d11754e8b5953ff075d0bf63b4
chenxiaonian <1012353276@qq.com>
2022-04-10 00:51:27 +0800

    1. 修改：customized/hal/gpio/gpio.h ，把一个对象看成 配置 和 操作 两部分，这样使用的话，就可以无视不同MCU的接口命名问题，起到一个中间层作用

--- customized/hal/gpio/gpio.h
+++ customized/hal/gpio/gpio.h
@@ -17,14 +17,18 @@ typedef struct
     uint32_t    flag;
 } gpio_cfg_t;
 
+typedef struct
+{
+    int (* gpio_config)     (gpio_cfg_t *p_cfg);
+    int (* gpio_output_set) (gpio_cfg_t *p_cfg, uint8_t value);
+    int (* gpio_input_get)  (gpio_cfg_t *p_cfg, uint8_t *p_value);
+} gpio_ops_t;
+
 
 typedef struct
 {
     gpio_cfg_t gpio_cfg;
-
-    int (* gpio_config)     (gpio_cfg_t *p_cfg);
-    int (* gpio_output_set) (gpio_cfg_t *p_cfg, uint8_t value);
-    int (* gpio_input_get)  (gpio_cfg_t *p_cfg, uint8_t *p_value);
+    gpio_ops_t gpio_ops;
 } gpio_object_t;
 
 
