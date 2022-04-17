e14b13fba4994057a7ffd7b9bd5ce345a46fd4cb
TimChanCHN <874797636@qq.com>
2022-04-17 18:16:06 +0800

    [MODIFY] update hk_systick

--- customized/hk_lib/f1/systick/hk_systick.c
+++ customized/hk_lib/f1/systick/hk_systick.c
@@ -2,6 +2,9 @@
 
 static uint32_t m_sys_tick;
 static uint32_t reload;
+static uint16_t sysclk = 72;
+static uint32_t fac_us = 0;
+static uint32_t fac_ms = 0;
 
 void systick_start(void)
 {
@@ -12,29 +15,27 @@ void systick_start(void)
 
 /*SysTick时钟源由HCLK/8提供，当系统频率为72MHz时最小计数周期为1/9MHz,
 计满9次为1us，fac_us以9为单位*/
-void hk_systick_init(systick_cfg_t *p_sys_config)
+void hk_systick_init(systick_cfg_t *p_cfg)
 {
 
-	SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8); //72MHz/8 = 9MHz
+	SysTick_CLKSourceConfig(p_cfg->clk_div); //72MHz/8 = 9MHz
 	NVIC_SetPriority(SysTick_IRQn, 3);
 
 
-	// 1. 设置delay_us/ms计数基准
-	p_sys_config->us_ticks = 9;
-
-	// 2. 获取systick
-	p_sys_config->p_systick = &m_sys_tick;
+	// 设置delay_us/ms计数基准
+	fac_us = p_cfg->ticks_per_us; //72/8 = 9,9次1uS
+	fac_ms = (uint16_t)fac_us * 1000;
 
 	// 3. 使能嘀嗒定时器
 	reload	=	SystemCoreClock / 8000000;
-	reload	*=	1000000 / p_sys_config->ticks_per_sec;
+	reload	*=	1000000 / p_cfg->ticks_per_sec;
 	systick_start();
 }
 
-void hk_delay_us(systick_cfg_t *p_sys_config, uint32_t us)
+void hk_delay_us(systick_cfg_t *p_cfg, uint32_t us)
 {
 	//	volatile uint32_t temp;
-	SysTick->LOAD = (uint32_t)(us * p_sys_config->us_ticks);
+	SysTick->LOAD = (uint32_t)fac_us;
 	SysTick->CTRL = 0x00; //停止计数
 	SysTick->VAL = 0x00;  //清空计数器
 	SysTick->CTRL = 0x01; //开始计数
@@ -51,13 +52,13 @@ void hk_delay_us(systick_cfg_t *p_sys_config, uint32_t us)
 /**
  * @brief 当ms过大，导致溢出，是个BUG
  */
-void hk_delay_ms(systick_cfg_t *p_sys_config, uint32_t ms)
+void hk_delay_ms(systick_cfg_t *p_cfg, uint32_t ms)
 {
 	uint32_t i = 0;
 	for(i = 0; i < ms; i++)
 	{
 		//	volatile uint32_t temp;
-		SysTick->LOAD = (uint32_t)(1000 * p_sys_config->us_ticks);
+		SysTick->LOAD = (uint32_t)fac_ms;
 		SysTick->CTRL = 0x00; //停止计数
 		SysTick->VAL = 0x00;  //清空计数器
 		SysTick->CTRL = 0x01; //开始计数
