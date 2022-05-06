162ca4a9c92c513d54d7811f500e35d6fb9ab6d4
chenxiaonian <1012353276@qq.com>
2022-05-06 23:37:52 +0800

    1. 新增 hal 和 hk-f0 的adcdemo

--- customized/hk_lib/f0/exit/hk_exit.c
+++ customized/hk_lib/f0/exit/hk_exit.c
@@ -3,13 +3,13 @@
 int hk_exit4_15_init(exit_cfg_t *p_exit_cfg)
 {
     hk_exit_pin_cfg *p_hk_exit_pin_cfg = (hk_exit_pin_cfg *)p_exit_cfg->p_pin_cfg;
-    hk_exit_cfg *p_hk_exit_cf = (hk_exit_cfg *)p_exit_cfg->p_exit_cfg;
+    hk_exit_cfg *p_hk_exit_cfg = (hk_exit_cfg *)p_exit_cfg->p_exit_cfg;
 
     EXTI_InitTypeDef   EXTI_InitStructure;
     GPIO_InitTypeDef   GPIO_InitStructure;
     NVIC_InitTypeDef   NVIC_InitStructure;
 
-    RCC_AHBPeriphClockCmd(p_hk_exit_pin_cfg->exit_pin_clk, ENABLE);
+    RCC_AHBPeriphClockCmd(p_hk_exit_pin_cfg->exit_pin_port_clk, ENABLE);
 
     /* Enable SYSCFG clock */
 //    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
@@ -21,9 +21,9 @@ int hk_exit4_15_init(exit_cfg_t *p_exit_cfg)
 
     SYSCFG_EXTILineConfig(p_hk_exit_pin_cfg->exit_pin_port_source, p_hk_exit_pin_cfg->exit_pin_source);
 
-    EXTI_InitStructure.EXTI_Line = p_hk_exit_cf->exit_line;  
-    EXTI_InitStructure.EXTI_Mode = p_hk_exit_cf->exit_mode;
-    EXTI_InitStructure.EXTI_Trigger = p_hk_exit_cf->exit_trigger;
+    EXTI_InitStructure.EXTI_Line = p_hk_exit_cfg->exit_line;  
+    EXTI_InitStructure.EXTI_Mode = p_hk_exit_cfg->exit_mode;
+    EXTI_InitStructure.EXTI_Trigger = p_hk_exit_cfg->exit_trigger;
     EXTI_InitStructure.EXTI_LineCmd = ENABLE;
     EXTI_Init(&EXTI_InitStructure);
 
@@ -40,11 +40,11 @@ int hk_exit4_15_enable(exit_cfg_t *p_exit_cfg)
 {
     EXTI_InitTypeDef   EXTI_InitStructure;
 
-    hk_exit_cfg *p_hk_exit_cf = (hk_exit_cfg *)p_exit_cfg->p_exit_cfg;
+    hk_exit_cfg *p_hk_exit_cfg = (hk_exit_cfg *)p_exit_cfg->p_exit_cfg;
 
-    EXTI_InitStructure.EXTI_Line = p_hk_exit_cf->exit_line;  
+    EXTI_InitStructure.EXTI_Line = p_hk_exit_cfg->exit_line;  
     EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-    EXTI_InitStructure.EXTI_Trigger = p_hk_exit_cf->exit_trigger;
+    EXTI_InitStructure.EXTI_Trigger = p_hk_exit_cfg->exit_trigger;
     EXTI_InitStructure.EXTI_LineCmd = ENABLE;
     EXTI_Init(&EXTI_InitStructure);
 
@@ -55,11 +55,11 @@ int hk_exit4_15_disable(exit_cfg_t *p_exit_cfg)
 {
     EXTI_InitTypeDef   EXTI_InitStructure;
 
-    hk_exit_cfg *p_hk_exit_cf = (hk_exit_cfg *)p_exit_cfg->p_exit_cfg;
+    hk_exit_cfg *p_hk_exit_cfg = (hk_exit_cfg *)p_exit_cfg->p_exit_cfg;
 
-    EXTI_InitStructure.EXTI_Line = p_hk_exit_cf->exit_line;  
-    EXTI_InitStructure.EXTI_Mode = p_hk_exit_cf->exit_mode;
-    EXTI_InitStructure.EXTI_Trigger = p_hk_exit_cf->exit_trigger;
+    EXTI_InitStructure.EXTI_Line = p_hk_exit_cfg->exit_line;  
+    EXTI_InitStructure.EXTI_Mode = p_hk_exit_cfg->exit_mode;
+    EXTI_InitStructure.EXTI_Trigger = p_hk_exit_cfg->exit_trigger;
     EXTI_InitStructure.EXTI_LineCmd = DISABLE;
     EXTI_Init(&EXTI_InitStructure);
 
@@ -70,11 +70,11 @@ extern exit_object_t g_exit4_15_obj;
 
 void EXTI4_15_IRQHandler(void)
 {
-  hk_exit_cfg *p_hk_exit_cf = (hk_exit_cfg *)g_exit4_15_obj.exit_cfg.p_exit_cfg;
+  hk_exit_cfg *p_hk_exit_cfg = (hk_exit_cfg *)g_exit4_15_obj.exit_cfg.p_exit_cfg;
 
-  if(EXTI_GetITStatus(p_hk_exit_cf->exit_line) != RESET)
+  if(EXTI_GetITStatus(p_hk_exit_cfg->exit_line) != RESET)
   {
-        EXTI_ClearITPendingBit(p_hk_exit_cf->exit_line);
+        EXTI_ClearITPendingBit(p_hk_exit_cfg->exit_line);
 
         g_exit4_15_obj.exit_ops.exit_irq_cb(&g_exit4_15_obj.exit_cfg);
   }
