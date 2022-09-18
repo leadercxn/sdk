ba6a82fd7ae459cfac54e10a86f9398e8f32c471
TimChanCHN <874797636@qq.com>
2022-09-19 01:29:48 +0800

    1. eeprom的读写不能连续读多个字节(即各个字节的读写间插入stop信号), 否则会读写失败

--- drivers/eeprom/24cxx.h
+++ drivers/eeprom/24cxx.h
@@ -3,9 +3,12 @@
 
 #include "i2c.h"
 
-uint8_t eeprom_write_reg(i2c_object_t *p_i2c_obj, uint16_t reg, uint8_t *buf, uint8_t len);
-void    eeprom_read_reg(i2c_object_t *p_i2c_obj, uint16_t reg, uint8_t *buf, uint8_t len);
 uint8_t eeprom_init(i2c_object_t *p_i2c_obj);
+uint8_t eeprom_check(i2c_object_t *p_i2c_obj);
 
+uint8_t eeprom_read_byte(i2c_object_t *p_i2c_obj, uint16_t reg, uint8_t *buf);
+uint8_t eeprom_write_byte(i2c_object_t *p_i2c_obj, uint16_t reg, uint8_t *buf);
+void eeprom_read(i2c_object_t *p_i2c_obj, uint16_t reg, uint8_t *buf, uint8_t len);
+void eeprom_write(i2c_object_t *p_i2c_obj, uint16_t reg, uint8_t *buf, uint8_t len);
 
 #endif
